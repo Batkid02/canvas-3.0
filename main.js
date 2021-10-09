c49553af870e5f8777d3828d9cffe20afffdd802
Batkid02 <85186074+Batkid02@users.noreply.github.com>
2021-10-09 16:48:33 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,18 +7,20 @@ var width = screen.width;
 new_width=screen.width-70;
 new_height=screen.height-300;
 
-if(width<992){
-document.getElementById("myCanvas").width=new_width;
-document.getElementById("myCanvas").height=new_height;
-document.body.style.overflow="hidden";
-}
+if(width < 992)
+	{
+	document.getElementById("myCanvas").width = new_width;
+    document.getElementById("myCanvas").height = new_height;
+    document.body.style.overflow = "hidden";
+    
+	}
+	canvas.addEventListener("touchstart", my_touchstart);
 
-canvas.addEventListener("touchstart",my_touchstart);
 function my_touchstart(e)
 {
     console.log("touchstart");
     color=document.getElementById("color").value;
-    width_of_line=document.getElementById("width").value;
+    width_of_line=document.getElementById("width_of_line").value;
     last_position_of_x=e.touches[0].clientX-canvas.offsetLeft;
     last_position_of_y=e.touches[0].clientY-canvas.offsetTop;
 
